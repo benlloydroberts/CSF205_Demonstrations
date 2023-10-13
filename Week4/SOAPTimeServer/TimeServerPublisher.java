cc773025462a304c56307b8a7a7835673bb282ae
Ben Lloyd-Roberts <benlloyd-roberts97@outlook.com>
2023-10-13 12:35:38 +0100

    Update TimeServerPublisher.java

--- Week4/SOAPTimeServer/TimeServerPublisher.java
+++ Week4/SOAPTimeServer/TimeServerPublisher.java
@@ -4,9 +4,9 @@ import jakarta.xml.ws.Endpoint;
 
 /**
  * This application publishes the web service whose
- * SIB is ch01.ts.TimeServerImpl. For now, the 
+ * SIB is timeServer.TimeServerImpl. For now, the 
  * service is published at network address 127.0.0.1.,
- * which is localhost, and at port number 9876, as this
+ * which is localhost, and at port number 8080, as this
  * port is likely available on any desktop machine. The
  * publication path is /ts, an arbitrary name.
  *
@@ -14,7 +14,7 @@ import jakarta.xml.ws.Endpoint;
  * In this two-argument version, the first argument is the
  * publication URL as a string and the second argument is
  * an instance of the service SIB, in this case
- * ch01.ts.TimeServerImpl.
+ * timeServer.TimeServerImpl.
  *
  * The application runs indefinitely, awaiting service requests.
  * It needs to be terminated at the command prompt with control-C
@@ -22,7 +22,7 @@ import jakarta.xml.ws.Endpoint;
  *
  * Once the applicatation is started, open a browser to the URL
  *
- *     http://127.0.0.1:9876/ts?wsdl
+ *     http://127.0.0.1:8080/ts?wsdl
  *
  * to view the service contract, the WSDL document. This is an
  * easy test to determine whether the service has deployed
